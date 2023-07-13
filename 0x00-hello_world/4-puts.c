fe811feb89c9080ea31c097daf8effdb767766f6
Ombati12467 <ombatibrian12467@gmail.com>
2023-07-13 14:34:19 -0700

    real coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: AlwaYS 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
+
