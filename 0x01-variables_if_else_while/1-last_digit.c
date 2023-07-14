4c820933294f569c6a8f9292f0824780362ea8d2
Ombati12467 <ombatibrian12467@gmail.com>
2023-07-14 16:29:25 -0700

    final changes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,30 +3,34 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: Get the last digit of a number after modulus
- * Return: Always 0
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
 	int n;
-	int lastnum;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastnum = n % 10;
-	if (lastnum > 5)
+
+	if ((n % 10) > 5)
 	{
-		printf("The last digit of %d is %d and is greater than 5\n", n, lastnum);
+		printf("Last digit of %d is %d and is greater than 5\n",
+			n, n % 10);
 	}
-	else if (lastnum == 0)
+	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
-		printf("The last digit of %d is %d and is 0\n", n, lastnum);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			n, n % 10);
 	}
 	else
 	{
-		printf("The last digit of %d is %d and less than 6 and not 0\n", n, lastnum);
+		printf("Last digit of %d is %d and is 0\n",
+			n, n % 10);
 	}
-	return (0);
-}
 
+	return (0);
+
+}
