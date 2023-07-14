4655ab088a61dbef9e6b08e8f457c1ec4e78448d
Ombati12467 <ombatibrian12467@gmail.com>
2023-07-14 15:04:56 -0700

    complete

---
+++ 2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: 'Print alphabets in lower case'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
