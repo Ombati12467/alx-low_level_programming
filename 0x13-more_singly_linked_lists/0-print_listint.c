03fda30a389deac1602eba30e09fea64c5a0a04e
Ombati12467 <ombatibrian12467@gmail.com>
2023-08-29 05:37:45 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,6 @@
 /**
  * print_listint - Prints all the elements of a listint_t list.
  * @h: Pointer to the head of the list.
- * 
  * Return: The number of nodes.
  */
 size_t print_listint(const listint_t *h)
@@ -17,5 +16,5 @@ size_t print_listint(const listint_t *h)
 		node_count++;
 	}
 
-	return node_count;
+	return (node_count);
 }
