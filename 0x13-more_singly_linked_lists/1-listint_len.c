75610e50c1b50d2a575655062ada70da6578a60f
Ombati12467 <ombatibrian12467@gmail.com>
2023-08-29 06:00:55 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 
 /**
  * listint_len - Returns the number of elements in a listint_t list.
- * @h: Pointer to the head of the list. 
+ * @h: Pointer to the head of the list.
  * Return: The number of nodes.
  */
 size_t listint_len(const listint_t *h)
