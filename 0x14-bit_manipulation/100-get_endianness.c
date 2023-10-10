c032a9fd32e11d42d1662cf9b7cfb24be4cc97c4
Ombati12467 <ombatibrian12467@gmail.com>
2023-10-09 23:28:02 -0700

    et 1

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness of the system.
+ * Return: 0 if big endian, 1 if little endian.
+ * If the least significant byte is at the lowest address, it's little endian
+ */
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *byte_ptr = (char *)&num;
+
+	if (*byte_ptr)
+		return (1);
+	else
+		return (0);
+}
